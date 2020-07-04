7dd769e4ceaba2ac515ddd98c3514d61cc766084
pad19 <pad19051999@gmail.com>
2020-07-04 11:47:34 +0300

    Add z1 task2

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
  *
  * @param {array} array
  */
-function showArray(array) {
+function getEven(array) {
+    array = array.filter(el => !(el % 2));
     alert(array);
 }
 
-showArray(arr);
+getEven(arr);
