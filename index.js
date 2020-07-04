7550c8803f455c99ed6c885818c574d79a91b0b4
pad19 <pad19051999@gmail.com>
2020-07-04 11:50:22 +0300

    Add z1 task4

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
  * @param {array} array
  * @returns {number}
  */
-function getSum(array) {
-    return array.reduce((acc, el) => acc + el, 0);
+function getMax(array) {
+    return Math.max(...array);
 }
 
-getSum(arr);
+getMax(arr);
