e24380a33862c93345754e285d603f379a18e6f8
pad19 <pad19051999@gmail.com>
2020-07-04 12:31:14 +0300

    Add z1 task5

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 /**
  *
  * @param {array} array
- * @returns {number}
+ * @param {number} element
+ * @param {number} index
+ * @returns {array}
  */
-function getMax(array) {
-    return Math.max(...array);
+function pushElementToArrayByIndex(array, element, index) {
+    array.length++;
+    for (let i = array.length - 1; i > index; i--) {
+        array[i] = array[i - 1];
+    }
+    array[index] = element;
+    return array;
 }
 
-getMax(arr);
+pushElementToArrayByIndex(arr, 5, 2);
