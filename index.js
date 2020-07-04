f286207d00e81e080dfc2e8c88d70b4ee7cec708
pad19 <pad19051999@gmail.com>
2020-07-04 11:48:34 +0300

    Add z1 task3

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 /**
  *
  * @param {array} array
+ * @returns {number}
  */
-function getEven(array) {
-    array = array.filter(el => !(el % 2));
-    alert(array);
+function getSum(array) {
+    return array.reduce((acc, el) => acc + el, 0);
 }
 
-getEven(arr);
+getSum(arr);
