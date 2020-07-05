4d4da10a2c7f03dc69c6aaaa06b5dbf525f759c1
pad19 <pad19051999@gmail.com>
2020-07-05 10:42:43 +0300

    Add z2 task1

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 'use strict';
 
-const arr = new Array(10).fill().map(() => Math.floor(Math.random() * 10));
+const arr1 = new Array(10).fill().map(() => Math.floor(Math.random() * 10));
+const arr2 = new Array(5).fill().map(() => Math.floor(Math.random() * 10));
 
 /**
  *
- * @param {array} array
- * @param {number} index
+ * @param {array} array1
+ * @param {array} array2
  * @returns {array}
  */
-function deleteElementToArrayByIndex(array, index) {
-    for (let i = index; i < array.length; i++) {
-        array[i] = array[i + 1];
+function concatArraysWithoutDoubles(array1, array2) {
+    const array3 = array1.concat(array2);
+    const resultArray = [];
+    
+    for (let i = 0; i < array3.length; i++) {
+        if (!resultArray.includes(array3[i])) {
+            resultArray.push(array3[i]);
+        }
     }
-    array.length--;
-    return array;
+
+    return resultArray;
 }
 
-deleteElementToArrayByIndex(arr, 4);
+concatArraysWithoutDoubles(arr1, arr2);
