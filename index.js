8684f095bd0a00a1ae0721da605a7bc84d5cc3ed
pad19 <pad19051999@gmail.com>
2020-07-05 11:01:20 +0300

    Add z2 task2

--- index.js
+++ index.js
@@ -9,17 +9,18 @@ const arr2 = new Array(5).fill().map(() => Math.floor(Math.random() * 10));
  * @param {array} array2
  * @returns {array}
  */
-function concatArraysWithoutDoubles(array1, array2) {
-    const array3 = array1.concat(array2);
+function concatArraysWithSameElementsWithoutDoubles(array1, array2) {
     const resultArray = [];
-    
-    for (let i = 0; i < array3.length; i++) {
-        if (!resultArray.includes(array3[i])) {
-            resultArray.push(array3[i]);
+
+    for (let i = 0; i < array1.length; i++) {
+        if (array2.includes(array1[i])) {
+            if (!resultArray.includes(array1[i])) {
+                resultArray.push(array1[i]);
+            }
         }
     }
 
     return resultArray;
 }
 
-concatArraysWithoutDoubles(arr1, arr2);
+concatArraysWithSameElementsWithoutDoubles(arr1, arr2);
